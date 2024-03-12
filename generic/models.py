a89b524778a99582bd8621a8bd03aecb6b3c8377
Uzayr Iqbal <hamiduzayr@gmail.com>
2024-03-12 13:34:41 +0530

    adding snippets and choosing them

--- generic/models.py
+++ generic/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.snippets.models import register_snippet
+# from wagtail.snippets.views.snippets import SnippetViewSet
 
 class GenericPage(Page):
     banner_title = models.CharField(max_length=100, default='welcome to home page')
@@ -17,9 +19,42 @@ class GenericPage(Page):
         on_delete=models.SET_NULL,
         related_name='+',
     )
+    author = models.ForeignKey(
+        "Author",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
         FieldPanel("introduction"),
         FieldPanel("banner_image"),
+        FieldPanel("author"),
     ]
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(blank=True, max_length=100)
+    company_name = models.CharField(blank=True, max_length=100)
+    company_url = models.URLField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete = models.SET_NULL,
+        null=True,
+        blank = False,
+        related_name = '+'
+    )
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("title"),
+        FieldPanel("company_name"),
+        FieldPanel("company_url"),
+        FieldPanel("image"),
+    ]
+
+    def __str__(self):
+        return self.name
